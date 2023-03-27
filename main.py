29c3f42efcff189cc6e1b37cac93afb9c45e55e6
danya <meowsweep@gmail.com>
2023-03-27 22:08:19 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+﻿from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+import os
+
+app = Flask(__name__)
+
+
+upload_folder = os.path.join('static', 'img')
+
+app.config['UPLOAD'] = upload_folder
+
+
+@app.route('/galery', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':    
+        file = request.files['img']
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD'], filename))
+        img = os.path.join(app.config['UPLOAD'], filename)
+        return render_template('index.html', img=img)
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
